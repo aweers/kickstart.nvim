9f6d7fd8d57a7a1c561aa043e6d12ea995cfcd9e
Alexander Weers <alexander.weers@tum.de>
2024-12-07 00:11:49 +0100

    Add precognition and screenkey

--- init.lua
+++ init.lua
@@ -990,6 +990,41 @@ require('lazy').setup({
       require('ufo').setup()
     end,
   },
+  {
+    'tris203/precognition.nvim',
+    event = 'VeryLazy',
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = 'Comment' },
+      hints = {
+        Caret = { text = '^', prio = 2 },
+        Dollar = { text = '$', prio = 1 },
+        MatchingPair = { text = '%', prio = 5 },
+        Zero = { text = '0', prio = 1 },
+        w = { text = 'w', prio = 10 },
+        b = { text = 'b', prio = 9 },
+        e = { text = 'e', prio = 8 },
+        W = { text = 'W', prio = 7 },
+        B = { text = 'B', prio = 6 },
+        E = { text = 'E', prio = 5 },
+      },
+      gutterHints = {
+        G = { text = 'G', prio = 10 },
+        gg = { text = 'gg', prio = 9 },
+        PrevParagraph = { text = '{', prio = 8 },
+        NextParagraph = { text = '}', prio = 8 },
+      },
+      disabled_fts = {
+        'startify',
+      },
+    },
+  },
+  {
+    'NStefan002/screenkey.nvim',
+    lazy = false,
+    version = '*', -- or branch = "dev", to use the latest commit
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
